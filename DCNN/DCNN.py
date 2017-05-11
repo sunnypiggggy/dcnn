436d6aef6452ce405a02058a29266b2a447b1580
sunnypiggggy <armhawk6@gmail.com>
2017-05-11 14:49:01 +0800

    5/11 1442

--- DCNN/DCNN.py
+++ DCNN/DCNN.py
@@ -1,4 +1,21 @@
+import librosa
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from audioFileUtility import *
 
 
+if __name__=='__main__':
+    #LoadAudioBatch(None)
+    #LoadAudioBatch(None,None)
+    fileAudioPath=getAudioPaths()
+    #for xx in fileAudioPath:
+    #    print(os.path.basename(xx))
+    #plt.plot( np.array( LoadAudioFiles(fileAudioPath[0])))
+    for x in range(10):
+         xx,sr= librosa.load(fileAudioPath[x])
+         plt.subplot(10,1,x+1)
+         plt.plot(xx) 
 
-if '__main__'==
+    plt.show()
+    print("The end")
---
+++ DCNN/audioFileUtility.py
@@ -0,0 +1,53 @@
+import librosa
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def LoadAudioFiles(filePath):
+   rawSound=[]
+   for fp in filePath:
+       X,SampleRate =librosa.load(fp)
+       rawSound.append(X)
+   return rawSound
+
+
+def LoadAudioBatch(classNum,numPerClass):
+    soundList=[]
+    rootPath=os.getcwd()
+    print(rootPath)
+    f= os.path.join(rootPath,'ESC-50-master')
+    #classDirNames=os.listdir(f)
+    classDirNames=[ name for name in os.listdir(f) if os.path.isdir(os.path.join(f,name))]
+    for subDirName in classDirNames:
+        print(subDirName)
+        fileRootPath=os.path.join(f,subDirName)
+        filePath=[ name for name in os.listdir(fileRootPath) if os.path.isfile(os.path.join(fileRootPath,name))]
+        for xx in range(len(filePath)):
+            print(os.path.join(fileRootPath,filePath[xx]))
+            soundList.append((subDirName,LoadAudioFiles(os.path.join(fileRootPath,filePath[xx]))))        
+    print(len(soundList))
+    
+def getAudioPaths():
+    filePathList=[]
+    rootPath=os.getcwd()
+    f= os.path.join(rootPath,'ESC-50-master')
+    classDirNames=[ name for name in os.listdir(f) if os.path.isdir(os.path.join(f,name))]
+    for subDirName in classDirNames:
+        #print(subDirName)
+        fileRootPath=os.path.join(f,subDirName)
+        filePath=[ name for name in os.listdir(fileRootPath) if os.path.isfile(os.path.join(fileRootPath,name))]
+        for xx in range(len(filePath)-1):
+            #print(os.path.join(fileRootPath,filePath[xx]))       
+            filePathList.append(os.path.join(fileRootPath,filePath[xx]))
+    return filePathList
+
+
+def plotWave(soundName,rawSound):
+    hfig=plt.figure()
+    i=1
+    for n,f in zip(soundName,rawSound):
+        plt.subplot(10,1,i)
+        librosa.display.waveplot(np.array(f),sr=22050)
+        plt.title(n.title())
+        i +=1;
+    plt.show()
